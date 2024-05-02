94342c4451cdfb26981c97663dc1d00d9595bf9d
OscarG <oscar.gamez88@gmail.com>
2024-05-01 17:31:24 -0700

    fixed clerk + shadcn darkmode problem

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body className={`font-sans ${inter.variable}`}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
